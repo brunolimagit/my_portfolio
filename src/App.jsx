0142283932af31154c793db0a0e7f0519a3e9e9a
brunolimagit <bru_jefferson@hotmail.com>
2026-02-11 00:51:24 -0300

    criando portfolio

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p className='text-2xl font-bold text-blue-500'>Hello Tailwind CSS test ok!</p>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
