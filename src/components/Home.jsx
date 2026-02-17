102b79641c3e6da490bcbda76201b7b923685c26
brunolimagit <bru_jefferson@hotmail.com>
2026-02-17 01:40:24 -0300

    apresentação na section principal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,24 +13,102 @@ export default function Home() {
         className="absolute inset-0  "
       />
 
+
+
       {/* CONTEÚDO NA FRENTE */}
-      <div className="relative z-10 h-full flex flex-col">
+      <div className="relative z-10 flex flex-col">
 
         <Header />
+       
+       <div className="z-20 mx-10 flex justify-between mt-5 items-start">
+         <div className="w-100 text-white font-['Bitcount_Grid_Double'] font-bold">
+          <h1 className="text-2xl ">
+           {"->"}FULL STACK DEVELOPER = {"{"} 
+          </h1>
 
-        <div className="flex flex-1 items-center justify-center mt-75">
-          <h1 className="font-['Bitcount_Grid_Double'] text-[#ff0000] text-[10rem] font-bold animate-float">
-           <Typewriter
-              words={["BRUNO DEV"]}
-              loop={true}
+          <p className="w-90">
+            <span className="text-[blue]">const</span> dev = {"{"} 
+
+            <p><span className="text-[#ff0000]">Stack: </span> <Typewriter
+            
+            words={[`Full Stack Developer`,]}
+              loop={1}
               cursor
-              typeSpeed={200}
+              typeSpeed={100}
             />
+            </p>
+
+            <p> <span className="text-[#ff0000]"> Especialidade:</span> <Typewriter
+            words={[`Aplicações Web Modernas`,]}
+              loop={1}
+              cursor
+              typeSpeed={100}
+            />
+            </p>
+
+            <p><span className="text-[#ff0000]">  Foco:</span> <Typewriter
+            words={[`Criar Soluções Escaláveis e Eficientes`,]}
+              loop={1}
+              cursor
+              typeSpeed={100}
+            />
+            </p>
+
+            <p><span className="text-[#ff0000]">Disponibilidade:</span>  <Typewriter
+            words={[`true`,]}
+              loop={1}
+              cursor
+              typeSpeed={100}
+            />
+            </p>
+            <p> <span className="text-[#ff0000]"> Objetivo:</span> <Typewriter
+            words={[`Construir Experiências Digitais Que Façam a Diferença`]}
+              loop={1}
+              cursor
+              typeSpeed={100}
+            />
+            </p>
+
+            <span className="text-2xl">{"};"}</span>
+          </p>
+          
+
+         
+        
+ </div>
+
+         <div className="w-77 text-white font-['Bitcount_Grid_Double'] font-bold mt-30">
+          <p><span className="text-[#ff0000]">  contato</span> {"( ) {"}
+           <span className="text-[blue]"> return </span><Typewriter
+            words={[`Vamos construir algo incrível juntos!`, ]}
+              loop={1}
+              cursor
+              typeSpeed={100}
+            />
+            </p>
+            
+            <span>{"};"}</span>
+           
+          <div className="font-bold font-['Bitcount_Grid_Double flex gap-5 mt-5 text-center ">
+            <button className="font-['Bitcount_Grid_Double'] w-30 text-[#ff0000] text-xl border-1 border-[#ff0000] rounded-[30px] p-2 hover:bg-[#ff0000]/15 hover:text-white hover:border-0">Contato</button>
+            <button className="font-['Bitcount_Grid_Double'] w-30 text-white text-xl  rounded-[30px] p-2 bg-[#ff0000]/15 hover:bg-white/0 hover:border-1 hover:text-[#ff0000] cursor-pointer">CV</button>
+          </div>
+
+         </div>
+
+       </div>
+
+
+        <div className="flex flex-1 items-center justify-center ">
+          <h1 className="font-['Bitcount_Grid_Double'] text-[#ff0000] text-[10rem] font-bold animate-float">
+      BRUNO DEV
           </h1>
         </div>
+       {/*MY INFO */}
 
       </div>
 
+     
     </section>
   );
 }
